82871d29c36e26fa907cebf1574c788d7d16867e
Joe Pasquantonio <joe.pasquantonio@gmail.com>
2019-05-09 15:36:45 -0400

    fixed all requests that weren't json and added Block, Address, Transaction, Mempool classes

--- blockstream.py
+++ blockstream.py
@@ -10,7 +10,7 @@ import requests
 
 class BlockExplorer:
     """
-    Interact with blockstream's block explorer
+    Wrapper class for blockstream's block explorer
     """
 
     def __init__(self):
@@ -35,7 +35,7 @@ class BlockExplorer:
         Request the raw transaction in hex
         """
         method = f'tx/{tx_id}/hex'
-        return self.make_json_request(method)
+        return self.make_request(method)
     
     def get_transaction_merkle_proof(self, tx_id):
         """
@@ -46,14 +46,14 @@ class BlockExplorer:
     
     def get_transaction_output_status(self, tx_id, vout):
         """
-        Requests the spending status of a transaction output
+        Request the spending status of a transaction output
         """
         method = f'tx/{tx_id}/outspend/{vout}'
-        return self.make_json_request(method)
+        return self.make_request(method)
     
     def get_all_transaction_outputs_statuses(self, tx_id):
         """
-        Requests the spending status of all transaction outputs
+        Request the spending status of all transaction outputs
         """
         method = f'tx/{tx_id}/outspends'
         return self.make_json_request(method)
@@ -66,52 +66,52 @@ class BlockExplorer:
     
     def get_address(self, address):
         """
-        Requests address information
+        Request address information
         """
         method = f'address/{address}'
         return self.make_json_request(method)
     
     def get_script_hash(self, script_hash):
         """
-        Requests information about an address/scripthash
+        Request information about an address/scripthash
         """
         method = f'scripthash/{script_hash}'
-        return self.make_json_request(method)
+        return self.make_request(method)
     
     def get_address_transactions(self, address):
         """
-        Requests all transactions for an address, newest first
+        Request all transactions for an address, newest first
         """
         method = f'address/{address}/txs'
         return self.make_json_request(method)
     
     def get_script_hash_transactions(self, script_hash):
         """
-        Requests all transactions for an address or script_hash,
+        Request all transactions for an address or script_hash,
         newest first
         """
         method = f'scripthash/{script_hash}/txs'
-        return self.make_json_request(method)
+        return self.make_request(method)
 
-    def get_confirmed_transaction_history(self, address, ls_tx_id):
+    def get_confirmed_transaction_history(self, address, ls_tx_id=''):
         """
-        Requests confirmed transaction history for an address, newest first
+        Request confirmed transaction history for an address, newest first
         25 per page
         """
         method = f'address/{address}/txs/chain/{ls_tx_id}'
         return self.make_json_request(method)
     
-    def get_confirmed_script_hash_transaction_history(self, sh, ls_tx_id):
+    def get_confirmed_script_hash_transaction_history(self, sh, ls_tx_id=''):
         """
-        Requests confirmed transaction history for a script_hash, newest first
+        Request confirmed transaction history for a script_hash, newest first
         25 per page
         """
         method = f'scripthash/{sh}/txs/chain/{ls_tx_id}'
-        return self.make_json_request(method)
+        return self.make_request(method)
     
     def get_address_mempool(self, address):
         """
-        Requests unconfirmed transaction history of an address, newest first
+        Request unconfirmed transaction history of an address, newest first
         up to 50 transactions no paging
         """
         method = f'address/{address}/txs/mempool'
@@ -119,7 +119,7 @@ class BlockExplorer:
     
     def get_script_hash_mempool(self, script_hash):
         """
-        Requests unconfirmed transaction history of a scripthash, newest first
+        Request unconfirmed transaction history of a scripthash, newest first
         up to 50 transactions no paging
         """
         method = f'scripthash/{script_hash}/txs/mempool'
@@ -127,7 +127,7 @@ class BlockExplorer:
 
     def get_address_utxo(self, address):
         """
-        Requests the list of unspent transaction outputs associated with
+        Request the list of unspent transaction outputs associated with
         an address
         """
         method = f'address/{address}/utxo'
@@ -135,7 +135,7 @@ class BlockExplorer:
     
     def get_script_hash_utxo(self, script_hash):
         """
-        Requests the list of unspent transaction outputs associated with
+        Request the list of unspent transaction outputs associated with
         a scripthash
         """
         method = f'scripthash/{script_hash}/utxo'
@@ -203,24 +203,24 @@ class BlockExplorer:
         Request the height of the last block
         """
         method = 'blocks/tip/height'
-        return self.make_json_request(method)
+        return self.make_request(method)
     
     def get_last_block_hash(self):
         """
         Request the hash of the last block
         """
         method = 'blocks/tip/hash'
-        return self.make_json_request(method)
+        return self.make_request(method)
     
     def get_mempool(self):
         """
-        Requests mempool backlog statistics
+        Request mempool backlog statistics
         """
-        return self.make_json_request('mempool/')
+        return self.make_request('mempool/')
     
     def get_mempool_transaction_ids(self):
         """
-        Requests the full list of transactions IDs currently in the mempool,
+        Request the full list of transactions IDs currently in the mempool,
         as an array
         """
         method = 'mempool/txids'
@@ -228,23 +228,26 @@ class BlockExplorer:
 
     def get_mempool_recent_transactions(self):
         """
-        Requests a list of the last 10 transactions to enter the mempool
+        Request a list of the last 10 transactions to enter the mempool
         """
         method = 'mempool/recent'
         return self.make_json_request(method)
     
     def get_fee_estimates(self):
         """
-        Requests an object where the key is the confirmation target (in number
+        Request an object where the key is the confirmation target (in number
         of blocks) and the value is estimated fee rate (in sat/vB)
         """
-        return self.make_json_request('fee-estimates/')
+        return self.make_request('fee-estimates/')
     
     def make_request(self, method):
         """
         Build a url and make an api request. use this for calls that don't 
         return in json format
         """
+        url = self.base_url + method
+        return requests.get(url).content.decode('utf-8')
+    
     def make_json_request(self, method):
         """
         Build a url and make an api request for json formatted data
@@ -252,11 +255,54 @@ class BlockExplorer:
         url = self.base_url + method
         return requests.get(url).json()
 
-    def get_blocks_on_date(self, dt):
-        """
-        Requests all blocks that were created on a certain date
-        """
-        pass
+
+class Block:
+    """Hold block data."""
+    def __init__(self, id, height, version, timestamp, tx_count, size, 
+                 weight, merkle_root, previous_block_hash, nonce, bits):
+        self.id = id
+        self.height = height
+        self.version = version
+        self.timestamp = timestamp
+        self.tx_count = tx_count
+        self.size = size
+        self.weight = weight
+        self.merkle_root = merkle_root
+        self.previous_block_hash = previous_block_hash
+        self.nonce = nonce
+        self.bits = bits
+
+
+class Address:
+    """Hold Address data."""
+    def __init__(self, address, chain_stats, mempool_stats):
+        self.address = address  # str
+        self.chain_stats = chain_stats  # dict
+        self.mempool_stats = mempool_stats  # dict
+
+
+class Transaction:
+    """Hold Transaction data."""
+    def __init__(self, id, version, locktime, vin, 
+                 vout, size, weight, fee, status):
+        self.id = id
+        self.version = version
+        self.locktime = locktime
+        self.vin = vin
+        self.vout = vout
+        self.size = size
+        self.weight = weight
+        self.fee = fee
+        self.status = status
+
+
+class Mempool:
+    """Hold Mempool data."""
+    def __init__(self, count, vsize, total_fee, fee_histogram):
+        self.count = count
+        self.vsize = vsize
+        self.total_fee = total_fee
+        self.fee_histogram = fee_histogram
 
 
 def print_dict(obj):
--- examples.py
+++ examples.py
@@ -3,7 +3,7 @@ Example uses for blockstream.info api wrapper class
 """
 import datetime
 
-from wrapper import BlockExplorer
+from blockstream import BlockExplorer, Block, Address, Transaction, Mempool
 
 
 def get_genesis_block(explorer):
@@ -27,6 +27,93 @@ explorer = BlockExplorer()
 
 print(get_genesis_block(explorer))
 print(get_genesis_block_by_hash(explorer))
+
+
+# get transaction
+tx_id = '73236c12b8595c85dc629e1e01e4a0938376fb6c219632a97429ab59d6c0fac3'
+transaction = explorer.get_transaction(tx_id)
+print(transaction)
+
+# get transaction status
+tx_status = explorer.get_transaction_status(tx_id)
+print(tx_status)
+
+# get transaction hex
+tx_hex = explorer.get_transaction_hex(tx_id)
+print(tx_hex)
+
+# get transaction merkle proof
+tx_merkle_proof = explorer.get_transaction_merkle_proof(tx_id)
+print(tx_merkle_proof)
+
+# get transaction output status
+vout = '1B2fFAimxLTDgvtLPs212pa3f7ymHdVQmV'
+tx_output_status = explorer.get_transaction_output_status(tx_id, vout)
+print(tx_output_status)
+
+# get all transaction outputs statuses
+all_outspends = explorer.get_all_transaction_outputs_statuses(tx_id)
+print(all_outspends)
+
+# post a transaction
+
+
+# get information about an address
+address = 'bc1qw6lwxz3lxtg969hx7g0qnxfnm7608m3nxu7far'
+addr_info = explorer.get_address(address)
+print(addr_info)
+
+# get address transactions
+txs = explorer.get_address_transactions(address)
+print(txs)
+
+# get confirmed transaction history
+confirmed_tx_hist = explorer.get_confirmed_transaction_history(address)
+print(confirmed_tx_hist)
+
+# get address mempool
+addr_mempool = explorer.get_address_mempool(address)
+print(addr_mempool)
+
+# get address utxo
+addr_utxo = explorer.get_address_utxo(address)
+print(addr_utxo)
+
+# get script has mempool
+#sh_mempool = explorer.get_script_hash_mempool(script_hash)
+#print(sh_mempool)
+
+# get information about a scripthash
+#script_hash = '3NukJ6fYZJ5Kk8bPjycAnruZkE5Q7UW7i8'
+#sh_info = explorer.get_script_hash(script_hash)
+#print(sh_info)
+
+# get script hash transactions
+#sh_txs = explorer.get_script_hash_transactions(script_hash)
+#print(sh_txs)
+
+# get confirmed script_hash transaction history
+#sh_tx_hist = explorer.get_confirmed_script_hash_transaction_history(script_hash)
+#print(sh_tx_hist)
+
+# get script hash utxo
+#sh_utxo = explorer.get_script_hash_utxo(script_hash)
+#print(sh_utxo)
+
+# get mempool
+mempool = explorer.get_mempool()
+print(mempool)
+
+# get mempool transactions IDs
+mempool_tx_ids = explorer.get_mempool_transaction_ids()
+print(mempool_tx_ids)
+
+# get mempool recent transactions 
+recent_mempool_txs = explorer.get_mempool_transaction_ids()
+print(recent_mempool_txs)
+
+# get fee estimates
+fee_estimates = explorer.get_fee_estimates()
 
 # get hash from height
 block_hash = explorer.get_block_hash_from_height('575165')
@@ -69,31 +156,12 @@ print(tip_height)
 tip_hash = explorer.get_last_block_hash()
 print(tip_hash)
 
+blocks = []
+for b in ten_blocks:
+    block = Block(b['id'], b['height'], b['version'], b['timestamp'], 
+            b['tx_count'], b['size'], b['weight'], b['merkle_root'], 
+            b['previous_block_hash'], b['nonce'], b['bits'])
+    blocks.append(block)
 
-# good example would be to show total transactions each day over past 10 days
-
-# Look up all blocks in past 10 days
-
-# divide those blocks into each day they occured
-
-# total the number of transactions in all those blocks
-
-# implement a function that gives you all blocks that occured on a certain day
-
-# a function that gives you all transactions that occured on a certain day
-
-# size of mempool given a specific day
-
-# total number of transactions
-
-# average number of transactions per day (since inception, 1 month, 1 year, 3 months, etc)
-
-# average size of block per day ...
-
-# average weight of block per day ...
-
-#### IN A SEPARATE PROGRAM
-
-# pull all that data in and match the data with price at the time (average, high, low)
-
-# build an API wrapper for a price data provider (maybe block stream)
+for block in blocks:
+    print(block.id, block.tx_count, block.timestamp)
